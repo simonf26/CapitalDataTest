9cb5f564810d4755b2dc67892efe4e58a90a0f56
Simon <simon.fremont26@gmail.com>
2024-11-01 16:07:51 +0100

    feat: implement convertRecordToJSON

--- Partie 1/pkg/csv.go
+++ Partie 1/pkg/csv.go
@@ -2,10 +2,18 @@ package csv
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 )
+
+type JSONRecord struct {
+	FirstName       string `json:"firstname"`
+	LastName        string `json:"lastname"`
+	Email           string `json:"email"`
+	InscriptionDate string `json:"inscription_date"`
+}
 
 // ParseCSV parses the given CSV file into a slice of slices of strings.
 func ParseCSV(filename string) ([][]string, error) {
@@ -41,8 +49,41 @@ func AppendCSV(filename string, records [][]string) ([][]string, error) {
 }
 
 func ConvertCSVToJSON(CSVpath, JSONFilename string) error {
+	if CSVpath == "" {
+		err := errNoPathGiven
+		return err
+	}
+
+	files, err := GetFiles(CSVpath)
+	if err != nil {
+		return err
+	}
+
+	var records [][]string
+
+	for _, file := range files {
+		records, err = AppendCSV(file, records)
+		if err != nil {
+			return err
+		}
+	}
 
 	return nil
+}
+
+func convertRecordToJSON(record []string) (*JSONRecord, error) {
+	if len(record) < 4 {
+		err := fmt.Errorf("%v : %v", errInvalidRecord, record)
+		return nil, err
+	}
+	jsonRecord := JSONRecord{
+		FirstName:       record[0],
+		LastName:        record[1],
+		Email:           record[2],
+		InscriptionDate: record[3],
+	}
+
+	return &jsonRecord, nil
 }
 
 // GetFiles is a helper function that returns a list containing the files from
@@ -64,12 +105,12 @@ func GetFiles(path string) ([]string, error) {
 
 		for _, file := range files {
 			if strings.HasSuffix(file.Name(), ".csv") {
-				csvFiles = append(csvFiles, file.Name())
+				csvFiles = append(csvFiles, filepath.Join(path, file.Name()))
 			}
 		}
 	} else {
 		if strings.HasSuffix(path, ".csv") {
-			csvFiles = append(csvFiles, filepath.Base(path))
+			csvFiles = append(csvFiles, path)
 		}
 	}
 
