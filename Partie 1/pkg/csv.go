42788b87a86dcbd69a8f95c42cd145e7505fac93
Simon <simon.fremont26@gmail.com>
2024-11-01 12:01:10 +0100

    feat: implement parseCSV

--- Partie 1/pkg/csv.go
+++ Partie 1/pkg/csv.go
@@ -1,5 +1,26 @@
 package csv
 
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
 func ParseCSV(filename string) ([][]string, error) {
-	return nil, nil
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var records [][]string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		records = append(records, strings.Split(line, ","))
+	}
+
+	return records[1:], nil
 }
